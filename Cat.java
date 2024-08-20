e6d1bc21b07c6341209f82898fcfdf4fdb15c35b
Nekoayah <133284929+AliyahAira@users.noreply.github.com>
2024-08-20 02:06:49 +0000

    TO BE EDIT

---
+++ Cat.java
@@ -0,0 +1,22 @@
+
+public class Cat implements Pet {
+    private Integer noOfLives;
+
+    public Integer getNoOfLives() {
+        return noOfLives;
+    }
+
+    public void setNoOfLives(Integer noOfLives) {
+        this.noOfLives = noOfLives;
+    }
+
+    @Override
+    public String makeSound() {
+        return "Meow, meow!";
+    }
+
+    @Override
+    public String play() {
+        return "Catching moving objects!";
+    }
+}
